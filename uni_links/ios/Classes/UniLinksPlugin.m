5e510b6b1ed3e09894b5873f6679bffd22b31158
Akiko Hiura <akiko.hiura@justincase-tech.com>
2023-12-02 09:33:54 +0900

    format

--- uni_links/ios/Classes/UniLinksPlugin.m
+++ uni_links/ios/Classes/UniLinksPlugin.m
@@ -9,47 +9,47 @@ static NSString *const kEventsChannel = @"uni_links/events";
 @end
 
 @implementation UniLinksPlugin {
-  FlutterEventSink _eventSink;
+    FlutterEventSink _eventSink;
 }
 
 static id _instance;
 
 + (UniLinksPlugin *)sharedInstance {
-  if (_instance == nil) {
-    _instance = [[UniLinksPlugin alloc] init];
-  }
-  return _instance;
+    if (_instance == nil) {
+        _instance = [[UniLinksPlugin alloc] init];
+    }
+    return _instance;
 }
 
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar> *)registrar {
-  UniLinksPlugin *instance = [UniLinksPlugin sharedInstance];
-
-  FlutterMethodChannel *channel =
-      [FlutterMethodChannel methodChannelWithName:kMessagesChannel
-                                  binaryMessenger:[registrar messenger]];
-  [registrar addMethodCallDelegate:instance channel:channel];
-
-  FlutterEventChannel *chargingChannel =
-      [FlutterEventChannel eventChannelWithName:kEventsChannel
+    UniLinksPlugin *instance = [UniLinksPlugin sharedInstance];
+    
+    FlutterMethodChannel *channel =
+    [FlutterMethodChannel methodChannelWithName:kMessagesChannel
                                 binaryMessenger:[registrar messenger]];
-  [chargingChannel setStreamHandler:instance];
-
-  [registrar addApplicationDelegate:instance];
+    [registrar addMethodCallDelegate:instance channel:channel];
+    
+    FlutterEventChannel *chargingChannel =
+    [FlutterEventChannel eventChannelWithName:kEventsChannel
+                              binaryMessenger:[registrar messenger]];
+    [chargingChannel setStreamHandler:instance];
+    
+    [registrar addApplicationDelegate:instance];
 }
 
 - (void)setLatestLink:(NSString *)latestLink {
-  static NSString *key = @"latestLink";
-
-  [self willChangeValueForKey:key];
-  _latestLink = [latestLink copy];
-  [self didChangeValueForKey:key];
-
-  if (_eventSink) _eventSink(_latestLink);
+    static NSString *key = @"latestLink";
+    
+    [self willChangeValueForKey:key];
+    _latestLink = [latestLink copy];
+    [self didChangeValueForKey:key];
+    
+    if (_eventSink) _eventSink(_latestLink);
 }
 
 - (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
+didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     NSURL *url = [launchOptions valueForKey:UIApplicationLaunchOptionsURLKey];
     if (url) {
         self.initialLink = [url absoluteString];
@@ -57,7 +57,7 @@ static id _instance;
     NSDictionary *activityDictionary = [launchOptions objectForKey:UIApplicationLaunchOptionsUserActivityDictionaryKey];
     if (activityDictionary) {
         NSUserActivity *userActivity = [activityDictionary objectForKey:@"UIApplicationLaunchOptionsUserActivityKey"];
-        if(userActivity) {
+        if (userActivity) {
             self.initialLink = [userActivity.webpageURL absoluteString];
         }
     }
@@ -66,18 +66,18 @@ static id _instance;
 }
 
 - (BOOL)application:(UIApplication *)app
-    openURL:(NSURL *)url
-    options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
     self.latestLink = [url absoluteString];
     return YES;
 }
 
 - (BOOL)application:(UIApplication *)application
-    continueUserActivity:(NSUserActivity *)userActivity
-    restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
+continueUserActivity:(NSUserActivity *)userActivity
+ restorationHandler:(void (^)(NSArray<id<UIUserActivityRestoring>> * _Nullable))restorationHandler {
     if ([userActivity.activityType isEqualToString:NSUserActivityTypeBrowsingWeb]) {
         self.latestLink = [userActivity.webpageURL absoluteString];
-        if(!self.initialLink) {
+        if (!self.initialLink) {
             self.initialLink = self.latestLink;
         }
         return YES;
@@ -86,24 +86,24 @@ static id _instance;
 }
 
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
-  if ([@"getInitialLink" isEqualToString:call.method]) {
-    result(self.initialLink);
-    // } else if ([@"getLatestLink" isEqualToString:call.method]) {
-    //     result(self.latestLink);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+    if ([@"getInitialLink" isEqualToString:call.method]) {
+        result(self.initialLink);
+        // } else if ([@"getLatestLink" isEqualToString:call.method]) {
+        //     result(self.latestLink);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 - (FlutterError *_Nullable)onListenWithArguments:(id _Nullable)arguments
                                        eventSink:(nonnull FlutterEventSink)eventSink {
-  _eventSink = eventSink;
-  return nil;
+    _eventSink = eventSink;
+    return nil;
 }
 
 - (FlutterError *_Nullable)onCancelWithArguments:(id _Nullable)arguments {
-  _eventSink = nil;
-  return nil;
+    _eventSink = nil;
+    return nil;
 }
 
 @end
